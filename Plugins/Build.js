97548f6c3be74d5668fd03320b06022308d93151
Shigma <1700011071@pku.edu.cn>
2018-04-10 00:49:35 +0800

    add build command

--- Plugins/Build.js
+++ Plugins/Build.js
@@ -1,20 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 const process = require('process');
-const tm = require('./Config.json');
 
-if (!tm.Kernel) {
-    console.log('Cannot find Thulium kernel.');
-    throw 'Cannot find Thulium kernel.';
+try {
+	const tm = require('./Config.json');
+	
+	if (!tm.Kernel) {
+		console.log('Cannot find Thulium kernel.');
+		throw 'Cannot find Thulium kernel.';
+	}
+	
+	const KernelPath = path.join(tm.Kernel + '/Library');
+	const Tokenizer = require(KernelPath + '/Token/Tokenizer');
+	
+	const input = process.argv.slice(2);
+	
+	const test = new Tokenizer(input[0], 'URL');
+	console.log(test.tokenize());
+} catch (e) {
+	console.log(e);
 }
-
-const KernelPath = path.join(tm.Kernel + '/Library');
-const Tokenizer = require(KernelPath + '/Token/Tokenizer');
-
-const input = process.argv.slice(2);
-
-if (input[0]) {
-    const test = new Tokenizer(input[0], 'URL');
-    console.log(test.tokenize());
-}
-
--- Plugins/Tokenize.py
+++ Plugins/Tokenize.py
@@ -1,10 +1,37 @@
 import sublime
 import sublime_plugin
+import subprocess
+from .Assets import *
 
 class tmTokenizeCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        self.window.status_message('hhhhhhh')
+	def run(self, args = ''):
+		self.sheet_view = self.window.active_sheet().view();
+		if not self.sheet_view: return
+
+		subprocess.Popen('taskkill /F /IM node.exe', shell = True)
+
+		command = 'node "{build}" "{file}" {args}'''.format(
+			build = path + 'Plugins/Build',
+			file = self.sheet_view.file_name(),
+			args = args
+		)
+		self.result = subprocess.Popen(command, 
+			stdout = subprocess.PIPE,
+			stderr = subprocess.PIPE,
+			shell = True
+		)
+
+		output = self.result.stdout.read().decode('utf-8')
+		self.window.status_message(command)
+
+		self.output_view = self.window.create_output_panel('tm')
+
+		self.output_view.run_command('append', {
+			'characters': output, 
+			'force': True, 
+			'scroll_to_end': True
+		})
+		self.window.run_command("show_panel", {"panel": "output.tm"})
 
 
-
-
+ 

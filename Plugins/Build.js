fd286ed469a8ce6a4640c670658f3a5cb51a5dd1
Shigma <1700011071@pku.edu.cn>
2018-04-10 23:46:02 +0800

    new feature: menu
    1. 添加了菜单栏命令
    2. 增加了tokenize结果的配色方案
    3. 对tokenizer进行了优化

--- Plugins/Build.js
+++ Plugins/Build.js
@@ -7,24 +7,26 @@ try {
   const tm = require('./Config.json');
   if (!tm.Kernel) throw 'Cannot find Thulium kernel.';
   
+  const StartTime = Date.now();
   const KernelPath = path.join(tm.Kernel + '/Library');
   const Tokenizer = require(KernelPath + '/Token/Tokenizer');
 
   const input = process.argv.slice(2);
   const test = new Tokenizer(input[0], 'URL');
   test.tokenize();
-
+  const EndTime = Date.now();
   // -d debug
   // -p play
 
   const result = `\
-  Tokenize Result:
+  [ Finished in ${EndTime - StartTime}ms. ]
+  [ ${test.Errors.length} error(s), ${test.Warnings.length} warning(s). ]
 
   - Title: ${test.Comment.map(line => `
     - ${line.trim()}`).join('')}
 
-  - Section Count: ${test.Sections.length}
-  
+  - Sections: ${test.Sections.length}
+
   - Warnings: ${test.Warnings.map(tok => tok.Err)}`;
   console.log(result);
 
--- Plugins/Tokenize.py
+++ Plugins/Tokenize.py
@@ -3,17 +3,31 @@ import sublime_plugin
 import subprocess
 from .Assets import *
 
-class tmTokenizeCommand(sublime_plugin.WindowCommand):
-	def run(self, args = ''):
-		self.sheet_view = self.window.active_sheet().view();
-		if not self.sheet_view: return
+def isThuliumFile(filename):
+	fileExt = os.path.splitext(filename)[1]
+	if fileExt == '.tm':
+		return True
+	else:
+		return False
+
+class tmExecuteCommand(sublime_plugin.WindowCommand):
+	def run(self, meta = ''):
+		self.sheet_view = self.window.active_sheet().view()
+		if not self.sheet_view: 
+			self.window.status_message('File was not found.')
+			return
+
+		file_name = self.sheet_view.file_name()
+		if not isThuliumFile(file_name): 
+			self.window.status_message('File extension should be ".tm".')
+			return
 
 		subprocess.Popen('taskkill /F /IM node.exe', shell = True)
 
-		command = 'node "{build}" "{file}" {args}'''.format(
+		command = 'node "{build}" "{file}" {args}'.format(
 			build = path + 'Plugins/Build',
-			file = self.sheet_view.file_name(),
-			args = args
+			file = file_name,
+			args = meta
 		)
 		self.result = subprocess.Popen(command,
 			stdout = subprocess.PIPE,
@@ -35,11 +49,7 @@ class tmTokenizeCommand(sublime_plugin.WindowCommand):
 		self.output_view.set_read_only(True)
 		self.output_view.set_scratch(True)
 		output_settings = self.output_view.settings()
-		output_settings.set("gutter", True)
-		output_settings.set("line_numbers", False)
-		output_settings.set("margin", 2)
-		output_settings.set("fold_buttons", True)
-		output_settings.set("fade_fold_buttons", True)
+		output_settings.set("gutter", False)
 		output_settings.set("draw_white_space", "none")
 
 		self.window.run_command("show_panel", {"panel": "output.tm"})
